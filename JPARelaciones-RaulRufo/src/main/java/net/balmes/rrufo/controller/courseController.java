c725839a1af64086016441c301814c15d4d291da
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2022-11-08 17:13:25 +0100

    Dia 2

--- JPARelaciones-RaulRufo/src/main/java/net/balmes/rrufo/controller/courseController.java
+++ JPARelaciones-RaulRufo/src/main/java/net/balmes/rrufo/controller/courseController.java
@@ -1,13 +1,20 @@
 package net.balmes.rrufo.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.balmes.rrufo.entity.course;
+import net.balmes.rrufo.error.apiError;
 import net.balmes.rrufo.repositorio.courseRepositorio;
+
 
 @RestController
 @RequestMapping("JPARelaciones")
@@ -17,7 +24,7 @@ public class courseController {
 	courseRepositorio cursosRep;
 	
 	// Muestra los cursos por Id
-	@GetMapping("curse/{id}")
+	@GetMapping("cursos/{id}")
 	public course getCursosId(@PathVariable long id) {
 		course cursos = cursosRep.findById(id).get();
 		
@@ -25,9 +32,21 @@ public class courseController {
 	}
 	
 	// Muestra todos los cursos
-	@GetMapping("curse")
+	@GetMapping("cursos")
 	public Iterable<course> getCursos() {
 		
 		return cursosRep.findAll();
 	}
+	/*
+	//Control del error
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<apiError> handlecourseNoEncontrado(Exception ex){
+		
+		apiError apierror = new apiError();
+		apierror.setEstado(HttpStatus.NOT_FOUND);
+		apierror.setFecha(LocalDateTime.now());
+		apierror.setMensaje(ex.getMessage());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apierror);
+	}*/
 }
---
+++ JPARelaciones-RaulRufo/src/main/java/net/balmes/rrufo/repositorio/courseMaterialRepositorio.java
@@ -0,0 +1,8 @@
+package net.balmes.rrufo.repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+import net.balmes.rrufo.entity.courseMaterial;
+
+public interface courseMaterialRepositorio extends CrudRepository<courseMaterial, Long >{
+
+}
